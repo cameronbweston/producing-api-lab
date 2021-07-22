477f6356766e1aa3885c065f11c5413913501031
Cameron Weston <cameronbweston@gmail.com>
2021-07-22 15:18:00 -0500

    Add db file

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import { fileURLToPath } from 'url'
 import cookieParser from 'cookie-parser'
 import logger from 'morgan'
 
+import './config/database.js'
 import { router as indexRouter } from './routes/index.js'
 import { router as usersRouter } from './routes/users.js'
 
